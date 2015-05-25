02f04f22a72bf479dab2a8fbf1209bcd527eb93f
kxp <zkxp2011@qq.com>
2015-05-25 16:41:18 +0800

    新建项目

---
+++ JavaSEDemo/src/ThinkingInJavaDemo/HelloDate.java
@@ -0,0 +1,17 @@
+package ThinkingInJavaDemo;
+import java.util.*;
+/**
+ * The first Thinking in Java example program
+ * @author admin
+ * @version 4.0
+ */
+public class HelloDate {
+	/** Entry point to class $application
+	 * @param args array of string arguments
+	 * @throws exceptions No exceptions thrown
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello ,it's");
+		System.out.println(new Date());
+	}
+}
